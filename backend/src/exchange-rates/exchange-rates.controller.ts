888655ecddb265504ccc1d7bbcfd21ec87b07d6f
kelvin <denlinato@gmail.com>
2025-03-19 13:45:11 +0000

    exchange rate and api

--- backend/src/exchange-rates/exchange-rates.controller.ts
+++ backend/src/exchange-rates/exchange-rates.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards , Get } from '@nestjs/common';
+import { ExchangeRatesService } from './exchange-rates.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('exchange-rates')
-export class ExchangeRatesController {}
+export class ExchangeRatesController {
+
+    constructor(private echangeRateService: ExchangeRatesService){
+
+    }
+
+    @UseGuards(AuthGuard)
+    @Get()
+    getExchangeRates(){
+
+       return  this.echangeRateService.getExchangeRates() 
+
+    }
+}
--- backend/src/exchange-rates/exchange-rates.service.ts
+++ backend/src/exchange-rates/exchange-rates.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import axios from 'axios';
+
 
 @Injectable()
-export class ExchangeRatesService {}
+export class ExchangeRatesService {
+  constructor() {}
+
+  async getExchangeRates() {
+    try {
+      const res = await axios.get('');
+
+      console.log(res);
+      return res;
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
